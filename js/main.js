f5e368c7272c0fea951643589dbe32ebe42f2fad
Emily <el3166@columbia.edu>
2022-11-12 20:34:58 -0500

    js script

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ function getAPIData(url) {
   return url
 }
 
-console.log(getAPIData())
+//
+
+var scrollpos = window.scrollY;
+var header = document.getElementById("header");
+var toToggle = document.querySelectorAll(".toggleColour");
+document.addEventListener("scroll", function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos > 10) {
+    for (var i = 0; i < toToggle.length; i++) {
+      toToggle[i].classList.add("opacity-0");
+      toToggle[i].classList.remove("text-white");
+    }
+  } else {
+    header.classList.remove("bg-white");
+    for (var i = 0; i < toToggle.length; i++) {
+      toToggle[i].classList.add("text-white");
+      toToggle[i].classList.remove("opacity-0");
+    }
+  }
+});
